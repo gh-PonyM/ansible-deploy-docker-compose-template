06b260608ccb7918e65f749ca1e070727f139fae
PonyM <dev@lksch.ch>
2024-01-16 21:42:27 +0100

    fix: compatibility with ansible > 2.10.8
    - threw bad escape char error
    - more friendly to read now
    - add template tests

--- tests/test_template.py
+++ tests/test_template.py
@@ -14,9 +14,7 @@ def test_create_role(temporary_directory, compose, fixture_path):
         f"out_roles_path={temporary_directory}",
         "-e",
         f"compose_file_path={fixture_path / compose}",
-        "-vvv"
     )
-    print(cmd)
     r = subprocess.run(cmd, encoding="utf-8", check=False, capture_output=True)
     print(r.stdout)
     assert r.returncode == 0
