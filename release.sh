1f23e2264791d473568ffd8108fa76fa3f81086b
PonyM <dev@lksch.ch>
2024-09-21 09:16:27 +0200

    fix sed command

--- release.sh
+++ release.sh
@@ -4,15 +4,18 @@ set -eu
 version="$1"
 commit_msg="Release v$version"
 
-CHANGES=$(git status --porcelain | wc -l)
-if [ "$CHANGES" -gt "0" ]; then
-  echo "Git is dirty. Commit all changes before doing a release"
-  git status --porcelain
-  exit 1
-fi
+check-git-dirty() {
+  CHANGES=$(git status --porcelain | wc -l)
+  if [ "$CHANGES" -gt "0" ]; then
+    echo "Git is dirty. Commit all changes before doing a release"
+    git status --porcelain
+    exit 1
+  fi
+}
 
+check-git-dirty
 poetry version $version
-sed -e -i "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $version/g" README.md
+sed -i -e "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $version/g" README.md
 poetry export -n -o scripts/requirements.txt
 git add .
 git commit -m "$commit_msg"
